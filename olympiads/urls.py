2fef41b7db4ce280b47872402ec98a4c1e383def
olympiadsgym <olympiadsgym101@gmail.com>
2026-05-28 00:30:45 +0800

    Fix: add missing change_password template, fix URL namespace conflict

--- olympiads/urls.py
+++ olympiads/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='/gym-mgmt-2026/', permanent=False)),
     path('gym-mgmt-2026/', include('core.urls')),
-    path('gym-mgmt-2026/', include('members.urls')),
+    path('gym-mgmt-2026/', include(('members.urls', 'members_admin'), namespace='members_admin')),
     path('portal/', include(('members.urls', 'members'), namespace='members')),
 ]
