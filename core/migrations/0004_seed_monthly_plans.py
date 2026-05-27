c405641d60f99114af73c57556aa0cda77004272
olympiadsgym <olympiadsgym101@gmail.com>
2026-05-27 22:10:41 +0800

    fix: remove old plans, seed monthly-only plans

---
+++ core/migrations/0004_seed_monthly_plans.py
@@ -0,0 +1,33 @@
+from django.db import migrations
+
+
+def seed_plans(apps, schema_editor):
+    MembershipPlan = apps.get_model('core', 'MembershipPlan')
+    # Remove all old plans that are not monthly
+    MembershipPlan.objects.exclude(
+        plan_name__in=['Monthly', 'Student Monthly']
+    ).delete()
+    # Upsert monthly plans
+    MembershipPlan.objects.update_or_create(
+        plan_name='Monthly',
+        defaults={'duration_days': 30, 'price': '600.00'},
+    )
+    MembershipPlan.objects.update_or_create(
+        plan_name='Student Monthly',
+        defaults={'duration_days': 30, 'price': '350.00'},
+    )
+
+
+def reverse_plans(apps, schema_editor):
+    pass  # Non-destructive reverse
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0002_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(seed_plans, reverse_plans),
+    ]
