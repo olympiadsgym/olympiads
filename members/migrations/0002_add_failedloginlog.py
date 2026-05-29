2b1d2d723828be37dbb2a2f618a575281389887d
olympiadsgym <olympiadsgym101@gmail.com>
2026-05-29 15:35:13 +0800

    Fix corrupted migrations - 0004 was duplicating 0002

--- members/migrations/0002_add_failedloginlog.py
+++ members/migrations/0002_add_failedloginlog.py
@@ -4,20 +4,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0001_initial'),
+        ('members', '0003_add_session_end_time'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='FailedLoginLog',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('account_identifier', models.CharField(max_length=254)),
-                ('ip_address', models.GenericIPAddressField(blank=True, null=True)),
-                ('timestamp', models.DateTimeField(auto_now_add=True)),
-            ],
-            options={
-                'ordering': ['-timestamp'],
-            },
+        migrations.AddField(
+            model_name='member',
+            name='updated_at',
+            field=models.DateTimeField(auto_now=True),
         ),
     ]
