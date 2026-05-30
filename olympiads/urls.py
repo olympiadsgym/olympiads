10ee0acb8aaa9bf0e271c36f3e4f9d329264874a
olympiadsgym <olympiadsgym101@gmail.com>
2026-05-31 07:16:01 +0800

    Fix: add admin-login shortcut URL

--- olympiads/urls.py
+++ olympiads/urls.py
@@ -2,8 +2,16 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Root → member portal login
     path('', RedirectView.as_view(url='/portal/login/', permanent=False)),
+
+    # Easy admin shortcut
+    path('admin-login/', RedirectView.as_view(url='/gym-mgmt-2026/', permanent=False)),
+
+    # Admin area
     path('gym-mgmt-2026/', include('core.urls')),
     path('gym-mgmt-2026/', include(('members.urls', 'members_admin'), namespace='members_admin')),
+
+    # Member portal
     path('portal/', include(('members.urls', 'members'), namespace='members')),
 ]
