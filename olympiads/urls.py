4703562c21aa2f94178e0edfcd3f2236672a4ef9
olympiadsgym <olympiadsgym101@gmail.com>
2026-05-31 07:21:04 +0800

    Fix: root URL redirects to admin login

--- olympiads/urls.py
+++ olympiads/urls.py
@@ -2,11 +2,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    # Root → member portal login
-    path('', RedirectView.as_view(url='/portal/login/', permanent=False)),
-
-    # Easy admin shortcut
-    path('admin-login/', RedirectView.as_view(url='/gym-mgmt-2026/', permanent=False)),
+    # Root → admin login
+    path('', RedirectView.as_view(url='/gym-mgmt-2026/', permanent=False)),
 
     # Admin area
     path('gym-mgmt-2026/', include('core.urls')),
